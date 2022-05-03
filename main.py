3732e92bf0e6977af365b18d93c0de1cd3c7aa8e
LTA <37591675+LikeToAccess@users.noreply.github.com>
2022-05-03 16:33:57 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# filename          : main.py
+# description       : Flask sample project
+# author            : Ian Ault
+# email             : liketoaccess@protonmail.com
+# date              : 05-03-2022
+# version           : v1.0
+# usage             : python main.py
+# notes             :
+# license           : MIT
+# py version        : 3.10.2 (must run on 3.6 or higher)
+#==============================================================================
+from flask import Flask
