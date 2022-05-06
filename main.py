2129b7da752f55c3797d86b53c473921b7257f80
LikeToAccess <375916737591675+LikeToAccess@users.noreply.github.com>
2022-05-06 01:07:47 -0500

    Added code

--- main.py
+++ main.py
@@ -10,4 +10,118 @@
 # license           : MIT
 # py version        : 3.10.2 (must run on 3.6 or higher)
 #==============================================================================
+import os
+import json
+import sqlite3
+from contextlib import closing
 from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+def read_json_file(filename, encoding="utf8"):
+	if not os.path.exists(filename): return []
+	with open(filename, "r", encoding=encoding) as file:
+		data = json.load(file)
+
+	return data
+
+def write_json_file(filename, data, encoding="utf8"):
+	with open(filename, "w", encoding=encoding) as file:
+		json.dump(data, file, indent=4, sort_keys=True)
+
+	return json.dumps(data, indent=4, sort_keys=True)
+
+def append_json_file(filename, data, encoding="utf8"):
+	existing_data = read_json_file(filename, encoding=encoding)
+	existing_data.append(data)
+	write_json_file(filename, existing_data, encoding=encoding)
+
+def read_database_file(path, table_name, args):
+	with closing(sqlite3.connect(path)) as connection:
+		with closing(connection.cursor()) as cursor:
+			rows = []
+			for key in args.keys():
+				rows += cursor.execute(
+					f"SELECT name, email, profile_picture FROM {table_name} WHERE {key} = ?",
+					(args[key],),
+				).fetchall()
+
+	return rows
+
+def create_database_file(path, table_name, data):
+	with closing(sqlite3.connect(path)) as connection:
+		with closing(connection.cursor()) as cursor:
+			cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({data})")
+
+def write_database_file(path, table_name, data):
+	with closing(sqlite3.connect(path)) as connection:
+		with closing(connection.cursor()) as cursor:
+			cursor.execute(f"INSERT INTO {table_name} VALUES {data}")
+			connection.commit()
+
+
+class Users(Resource):
+	def __init__(self):
+		create_database_file("./data/users.db", "users", "name TEXT, email TEXT, profile_picture TEXT")
+
+	def get(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument("email", required=False, type=str, help="Email of the user", location="args")
+		parser.add_argument("name", required=False, type=str, help="Real name of the user", location="args")
+		print("GET")
+		args = parser.parse_args()
+		if args:
+			data = read_database_file("./data/users.db", "users", args)
+			if data:
+				return {"data": data}, 200
+			return {"message": f"{args['email']} does not exist"}, 404
+		return {"message": "An email is required"}, 409
+
+	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument("name", required=True, type=str, help="Real name of the user", location="args")
+		parser.add_argument("email", required=True, type=str, location="args")
+		parser.add_argument("profile_picture", required=False, type=str, location="args")
+		args = parser.parse_args()
+
+
+		data = read_database_file("./data/users.db", "users", {"email": args["email"]})
+		if data:
+			return {
+				"message": f"{args['email']} already exists"
+			}, 409
+
+		data = args["name"], args["email"], args["profile_picture"]
+		# write_database_file("./data/users.db", "users", data)
+		write_database_file(
+			"./data/users.db",
+			"users",
+			f"('{args['name']}', '{args['email']}', "+f"'{args['profile_picture']}')" if
+		)
+
+		return {
+			"name":            args["name"],
+			"email":           args["email"],
+			"profile_picture": args["profile_picture"],
+		}, 200
+
+class Reviews(Resource):
+	def get(self):
+		return {"message": "Not implemented"}, 501
+
+	def post(self):
+		return {"message": "Not implemented"}, 501
+
+
+def main():
+	api.add_resource(Users, "/users")
+	api.add_resource(Reviews, "/reviews")
+	app.run(debug=True)
+
+
+if __name__ == "__main__":
+	main()
